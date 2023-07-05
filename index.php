65a1fe1940d17344f406ed63ab5d6e776285470c
manuelcamaioni <camaioni.manuel@gmail.com>
2023-07-05 14:37:47 +0200

    added basic style of index.php

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
     <!-- BOOTSTRAP v5.2 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
     <link rel="stylesheet" href="./styles/style.css">
     <script src="./script/main.js" defer></script>
 
@@ -15,7 +17,18 @@
 </head>
 <body>
     <div id="app">
-        
+        <header>
+            <nav>
+                <div class="logo-container my-container">
+                    <i class="fa-brands fa-spotify"></i>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <div class="my-container">
+                
+            </div>
+        </main>
     </div>
 </body>
 </html>
