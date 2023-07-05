b86a76a355e4eb3d2bb5441c16cbc349faa9f2ce
manuelcamaioni <camaioni.manuel@gmail.com>
2023-07-05 13:04:52 +0200

    init index.php, server.php, databse with json, main.js and style.css

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- VUE3 -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!-- BOOTSTRAP v5.2 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="./styles/style.css">
+    <script src="./script/main.js" defer></script>
+
+    <title>Spotify Records</title>
+</head>
+<body>
+    <div id="app">
+        
+    </div>
+</body>
+</html>
